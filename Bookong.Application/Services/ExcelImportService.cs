c23475b87a337b197cce8c2c5ceec16f0918d436
Matyáš Sýs <sysm22@spstrutnov.cz>
2025-10-19 18:35:42 +0200

    File structure for importing excel lists

---
+++ Bookong.Application/Services/ExcelImportService.cs
@@ -0,0 +1,17 @@
+﻿using Bookong.Application.DTOs;
+using Bookong.Application.Services.Interfaces;
+
+namespace Bookong.Application.Services
+{
+    internal class ExcelImportService : IExcelImportService
+    {
+        public List<ImportBookDto> ParseBooks(Stream excelStream)
+        {
+            // This service will handle parsing the excel file and returning list of ImportBookDto
+
+            // This needs to be implemented in order for the ImportBooksFromExcelUseCase to work
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Bookong.Application/UseCases/ImportBooksFromExcelUseCase.cs
@@ -0,0 +1,19 @@
+﻿using Bookong.Application.DTOs;
+using Bookong.Application.Services;
+using Bookong.Application.UseCases.Interfaces;
+using System.Collections.Generic;
+
+namespace Bookong.Application.UseCases
+{
+    public class ImportBooksFromExcelUseCase : IImportBooksFromExcelUseCase
+    {
+        public Task<GenericResponse> ExecuteAsync(Stream excelStream)
+        {
+            // Uzivatel nahraje ten excel
+            ExcelImportService excelImportService = new ExcelImportService();
+            List<ImportBookDto> KnizkyzExccelu = excelImportService.ParseBooks(excelStream);
+            // Cteme ten excel
+            throw new NotImplementedException();
+        }
+    }
+}
