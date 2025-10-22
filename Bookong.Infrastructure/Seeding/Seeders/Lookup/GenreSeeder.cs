3feb31a009dfd9c7ae8825ff306157658715be5f
Matyáš Sýs <sysm22@spstrutnov.cz>
2025-10-22 21:46:05 +0200

    Pridani CLI projektu se seed nastrojem

---
+++ Bookong.Infrastructure/Seeding/Seeders/Lookup/GenreSeeder.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Bookong.Domain.Entities;
+using Bookong.Infrastructure.Data;
+
+namespace Bookong.Infrastructure.Seeding.Seeders.Lookup;
+
+public class GenreSeeder : ISeeder
+{
+    public string Name => "Genres";
+    public int Order => 110;
+    public bool IsDemoData => false;
+
+    // Representative genres used in the dataset
+    private static readonly string[] Defaults = new[]
+    {
+        "Román", "Novela", "Povídka", "Románová kronika", "Historický román", "Soubor povídek", "Soubor povìstí", "Drama"
+    };
+
+    public async Task SeedAsync(BookongDbContext db, CancellationToken cancellationToken = default)
+    {
+        foreach (var name in Defaults)
+        {
+            if (!db.Genres.Any(k => k.Name == name))
+            {
+                db.Genres.Add(new Genre { PublicId = Guid.NewGuid(), Name = name });
+            }
+        }
+        await db.SaveChangesAsync(cancellationToken);
+    }
+}
