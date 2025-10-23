25c6a97134f4cdc5d5bf5bd6176ed9018432a1df
František Brikner <briknerf22@spstrutnov.cz>
2025-10-23 19:45:19 +0200

    začátek práce na přidávání knih do seznamu na str. maturita

--- Bookong.Web/Program.cs
+++ Bookong.Web/Program.cs
@@ -1,39 +1,39 @@
-using Bookong.Infrastructure.Data;
-using Bookong.Web.Components;
-using Microsoft.EntityFrameworkCore;
-using Bookong.Domain.Interfaces;
-using Bookong.Infrastructure.Services;
+﻿using Bookong.Application.UseCases.Queries;
 using Bookong.Application.UseCases.Queries.Interfaces;
-using Bookong.Application.UseCases.Queries;
+using Bookong.Domain.Interfaces;
+using Bookong.Infrastructure.Data;
+using Bookong.Infrastructure.Services;
+using Bookong.Web.Components;
+using Bookong.Web.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Připojení k databázi
 builder.Services.AddDbContext<BookongDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Unit of Work
+// Unit of Work a Queries
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-// Application UseCases, Queries
 builder.Services.AddScoped<IGetAvailableBooksQuery, GetAvailableBooksQuery>();
 
-// Add services to the container.
+// Singleton pro uchování knih pro maturitu
+builder.Services.AddSingleton<MaturitaService>();
+
+// Blazor komponenty
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
---
+++ Bookong.Web/Services/MaturitaServices.cs
@@ -0,0 +1,46 @@
+﻿using Bookong.Application.DTOs;
+using Bookong.Domain.Entities;
+using Bookong.Infrastructure.Data;
+
+public class MaturitaService
+{
+    private readonly BookongDbContext _context;
+
+    public MaturitaService(BookongDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<BookListItemDto>> GetBooksAsync()
+    {
+        // join s knihami podle PublicId
+        return await _context.MaturitaBooks
+                             .Include(m => m.Book)
+                             .Select(m => new BookListItemDto
+                             {
+                                 PublicId = m.Book.PublicId,
+                                 Title = m.Book.Title,
+                                 AuthorFullName = m.Book.AuthorFullName,
+                                 Genre = m.Book.Genre,
+                                 Kind = m.Book.Kind
+                             }).ToListAsync();
+    }
+
+    public async Task AddBookAsync(BookListItemDto book)
+    {
+        if (!await _context.MaturitaBooks.AnyAsync(m => m.Book.PublicId == book.PublicId))
+        {
+            _context.MaturitaBooks.Add(new MaturitaBook { BookPublicId = book.PublicId });
+            await _context.SaveChangesAsync();
+        }
+    }
+}
+
+public class MaturitaBook
+{
+    public int Id { get; set; }
+    public Guid BookPublicId { get; set; }
+
+    // navigační vlastnost
+    public Book? Book { get; set; }
+}
