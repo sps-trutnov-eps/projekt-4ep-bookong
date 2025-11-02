c119b7a63f0cd0dba3a144e818ed9a9a2ed5b794
František Brikner <briknerf22@spstrutnov.cz>
2025-11-02 19:50:00 +0100

    úprava souboru program.cs

--- Bookong.Web/Program.cs
+++ Bookong.Web/Program.cs
@@ -1,30 +1,27 @@
-﻿using System;
-using Bookong.Application.UseCases.Commands;
-using Bookong.Application.UseCases.Commands.Interfaces;
-using Bookong.Application.UseCases.Queries;
-using Bookong.Application.UseCases.Queries.Interfaces;
-using Bookong.Domain.Entities;
-using Bookong.Domain.Interfaces;
-using Bookong.Infrastructure.Data;
-using Bookong.Infrastructure.Services;
+﻿using Bookong.Infrastructure.Data;
 using Bookong.Web.Components;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using System.Linq;
+using Bookong.Domain.Interfaces;
+using Bookong.Infrastructure.Services;
+using Bookong.Application.UseCases.Queries.Interfaces;
+using Bookong.Application.UseCases.Queries;
+using Bookong.Application.UseCases.Commands.Interfaces;
+using Bookong.Application.UseCases.Commands;
 using Bookong.Application.Services.Interfaces;
 using Bookong.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Připojení k databázi
 builder.Services.AddDbContext<BookongDbContext>(options =>
- options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Unit of Work a Queries
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IGetAvailableBooksQuery, GetAvailableBooksQuery>();
-builder.Services.AddScoped<IAddBookToMaturitaSelectionCommand, AddBookToMaturitaSelectionCommand>();
 builder.Services.AddScoped<IGetMaturitaBookSelectionQuery, GetMaturitaBookSelectionQuery>();
+
+// Commands
+builder.Services.AddScoped<IAddBookToMaturitaSelectionCommand, AddBookToMaturitaSelectionCommand>();
 builder.Services.AddScoped<IRemoveBookFromMaturitaSelectionCommand, RemoveBookFromMaturitaSelectionCommand>();
 
 // Session services
@@ -34,45 +31,23 @@ builder.Services.AddSession(options =>
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
-
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<ISessionService, SessionService>();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
- .AddInteractiveServerComponents();
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Seed only in Development
-if (app.Environment.IsDevelopment())
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
- using (var scope = app.Services.CreateScope())
- {
- var db = scope.ServiceProvider.GetRequiredService<BookongDbContext>();
- if (!db.Users.Any())
- {
- db.Users.Add(new User
- {
- PublicId = Guid.NewGuid(),
- SamAccountName = "devuser",
- ObjectGuid = Guid.NewGuid(),
- Name = "Dev User",
- Email = "dev@example.com",
- OrganizationalUnit = "OU=Users"
- });
- db.SaveChanges();
- }
- }
-}
-else
-{
- app.UseExceptionHandler("/Error", createScopeForErrors: true);
- app.UseHsts();
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseSession();
 
 // Development only middleware to set a test user in session
@@ -82,9 +57,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
- .AddInteractiveServerRenderMode();
+    .AddInteractiveServerRenderMode();
 
 app.Run();
