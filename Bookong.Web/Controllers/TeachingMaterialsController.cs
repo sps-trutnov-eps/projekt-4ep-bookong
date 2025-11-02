3744a189e195973763ebf3067a37f15ebc3b4cc2
Josef Budinský <121012357+pepicek160@users.noreply.github.com>
2025-11-02 21:20:47 +0100

    zkouška 2

--- Bookong.Web/Controllers/TeachingMaterialsController.cs
+++ Bookong.Web/Controllers/TeachingMaterialsController.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using Bookong.Application.UseCases.Interfaces;
-using Bookong.Application.DTOs;
+using Bookong.Web.Models;
 
 namespace Bookong.Web.Controllers
 {
@@ -8,34 +7,32 @@ namespace Bookong.Web.Controllers
     [Route("api/[controller]")]
     public class TeachingMaterialsController : ControllerBase
     {
-        private readonly ICreateTeachingMaterialUseCase _createTeachingMaterialUseCase;
-
-        public TeachingMaterialsController(ICreateTeachingMaterialUseCase createTeachingMaterialUseCase)
-        {
-            _createTeachingMaterialUseCase = createTeachingMaterialUseCase;
-        }
-
-        [HttpPost("create")]
-        public async Task<IActionResult> Create([FromBody] CreateTeachingMaterialDto dto)
-        {
-            var result = await _createTeachingMaterialUseCase.ExecuteAsync(dto);
-            return Ok(result);
-        }
-
         [HttpGet("folders")]
         public IActionResult GetFolders()
         {
-            var folders = new[]
+            var folders = new List<FolderDto>
             {
-                new {
+                new FolderDto
+                {
                     Name = "Angličtina",
                     Description = "Materiály pro AJ",
                     CreatedBy = "Novák",
-                    Files = new object[] {},
-                    Links = new object[] {},
-                    StudentMaterials = new object[] {}
+                    Files = new List<FileDto>
+                    {
+                        new FileDto { FileName = "Grammar.pdf", FileType = "PDF" },
+                        new FileDto { FileName = "Vocabulary.docx", FileType = "Word" }
+                    },
+                    Links = new List<LinkDto>
+                    {
+                        new LinkDto { Name = "Online procvičování", Url = "https://example.com/english" }
+                    },
+                    StudentMaterials = new List<StudentMaterialDto>
+                    {
+                        new StudentMaterialDto { Title = "Lesson 1", Description = "Úvod do gramatiky" }
+                    }
                 }
             };
+
             return Ok(folders);
         }
     }
